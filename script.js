f4f7671f970939ad1a391054bc113cc383fdfc8e
Ayo-Show <sowunmi30@gmail.com>
2022-02-11 11:11:22 -0500

    Updated code to display alert when there are no results

--- script.js
+++ script.js
@@ -3,14 +3,29 @@ async function handleSubmit(event) {
   const inputValue = document.querySelector(".js-search-input").value;
   const searchQuery = inputValue.trim();
 
+  const searchResults = document.querySelector(".js-search-results");
+
+  // Clear the previous results
+
+  searchResults.innerHTML = "";
+
+  const spinner = document.querySelector(".js-spinner");
+
+  spinner.classList.remove("hidden");
+
   try {
     const results = await searchWikipedia(searchQuery);
+    if (results.query.searchinfo.totalhits === 0) {
+      alert("No results found. Try different keywords");
+      return;
+    }
 
-    console.log(results);
+    displayResults(results);
   } catch (err) {
     console.log(err);
-
     alert("Failed to search wikipedia");
+  } finally {
+    spinner.classList.add("hidden");
   }
 }
 
@@ -24,6 +39,28 @@ async function searchWikipedia(searchQuery) {
   return json;
 }
 
-const form = document.querySelector('.js-search-form');
-form.addEventListener('submit', handleSubmit);
+const form = document.querySelector(".js-search-form");
+form.addEventListener("submit", handleSubmit);
 
+function displayResults(results) {
+  // Get a reference to the `.js-search-results` element
+  const searchResults = document.querySelector(".js-search-results");
+
+  // Iterate over the `search` array. Each nested object in the array can be
+  // accessed through the `result` parameter
+  results.query.search.forEach((result) => {
+    const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+    // Append the search result to the DOM
+    searchResults.insertAdjacentHTML(
+      "beforeend",
+      `<div class="result-item">
+        <h3 class="result-title">
+          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+        </h3>
+        <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+        <span class="result-snippet">${result.snippet}</span><br>
+      </div>`
+    );
+  });
+}
